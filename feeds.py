3b2d6a840d77d0ee42f82466557130d327de6291
Sean Talts <sean@lot18.com>
2013-06-05 00:39:53 -0400

    Allow 40-43 streets

--- feeds.py
+++ feeds.py
@@ -4,7 +4,8 @@ email_addresses = ["xitrium@gmail.com", "adared@gmail.com", "vireson@gmail.com"]
 ASTORIA_FILTERS = (
     #RegexFilter("(((2[8-9]|3[04-9])th|31st|32nd|33rd).)|((2[8-9]|3[0-9]).?st)"),
     RegexFilter('broadway|((30(th)?|31(st)?) ave)'),
-    InverseRegexFilter('[4-9][0-9].{0,4}st'),
+    InverseRegexFilter('[5-9][0-9].{0,4}st'),
+    InverseRegexFilter('4[4-9].{0,4}st'),
     InverseRegexFilter('(1[0-9]|2[0-6]).{0,4}st'),
     InverseRegexFilter('2[458](th)?\s*ave'),
     InverseRegexFilter('available\s*june'),
