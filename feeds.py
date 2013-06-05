8e0673b03b688c84ed3de3cc289aa1c5f5f0aaa4
Sean Talts <sean@lot18.com>
2013-06-04 21:55:31 -0400

    add 4br search

--- feeds.py
+++ feeds.py
@@ -1,8 +1,6 @@
 from filtr import RegexFilter, InverseRegexFilter
 email_addresses = ["xitrium@gmail.com", "adared@gmail.com", "vireson@gmail.com"]
-
-feeds = {"http://newyork.craigslist.org/search/aap/que?bedrooms=3&maxAsk=3000&minAsk=1000&query=astoria%20-rego%20-ditmars%20-elmhurst%20-ridgewood%20-kew%20-forest%20-corona%20-woodside%20-jackson%20-flushing&srchType=A&format=rss":
-         (
+ASTORIA_FILTERS = (
              #RegexFilter("(((2[8-9]|3[04-9])th|31st|32nd|33rd).)|((2[8-9]|3[0-9]).?st)"),
              RegexFilter('broadway|((30(th)?|31(st)?) ave)'),
              InverseRegexFilter('[4-9][0-9].{0,4}st'),
@@ -10,4 +8,8 @@ feeds = {"http://newyork.craigslist.org/search/aap/que?bedrooms=3&maxAsk=3000&mi
              InverseRegexFilter('2[458](th)?\s*ave'),
              InverseRegexFilter('available\s*june'),
          )
+feeds = {"http://newyork.craigslist.org/search/aap/que?bedrooms=3&maxAsk=3000&minAsk=1000&query=astoria%20-rego%20-ditmars%20-elmhurst%20-ridgewood%20-kew%20-forest%20-corona%20-woodside%20-jackson%20-flushing&srchType=A&format=rss":
+         ASTORIA_FILTERS,
+         "http://newyork.craigslist.org/search/aap/que?bedrooms=4&maxAsk=4200&query=astoria%20-rego%20-ditmars%20-elmhurst%20-ridgewood%20-kew%20-forest%20-corona%20-woodside%20-jackson%20-flushing&srchType=A&useMap=1&format=rss":
+         ASTORIA_FILTERS,
          }
